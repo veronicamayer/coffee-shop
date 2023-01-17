9c09af729dacdd352c33c21240835576e8bcb312
veronicamayer <mayerveronica.vm@gmail.com>
2023-01-17 21:15:28 +0100

    test mobile

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+/* Burger Menu Toggle */
+var buttons = document.getElementsByClassName("toggleNav");
+var nav = document.getElementById("nav");
+var isVisible = false;
+
+for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        if(isVisible) {
+            nav.style.display = "none";
+            isVisible = false;
+        } else {
+            nav.style.display = "flex";
+            isVisible = true;
+        }
+    });
+}
+
+/* Carousel */
+sliderContainer.addEventListener("touchstart", (e) => {
+    pressed = true;
+    startX = e.touches[0].clientX - innerSlider.offsetLeft;
+    sliderContainer.style.cursor = "grabbing";
+});
+
+sliderContainer.addEventListener("touchmove", (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+
+    x = e.touches[0].clientX;
+
+    innerSlider.style.left = `${x - startX}px`;
+});
+
+sliderContainer.addEventListener("touchend", () => {
+    sliderContainer.style.cursor = "grab";
+    pressed = false;
+});
